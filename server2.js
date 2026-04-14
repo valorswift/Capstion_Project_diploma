6e2c3fb60d6bd70f5daff362f2c616fd94e97fb2
valorswift <paau1508@gmail.com>
2026-04-15 00:00:49 +0530

    Update server2.js

--- server2.js
+++ server2.js
@@ -29,12 +29,15 @@ app.get("/", (req, res) => {
 // ⚠️ Replace with your Gmail + App Password
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
+    port: 465,
+    secure: true,
    auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
 },
+  connectionTimeout: 60000,   // 🔥 add this
+  greetingTimeout: 60000,
+  socketTimeout: 60000,
     tls: { rejectUnauthorized: false }
 });
 
