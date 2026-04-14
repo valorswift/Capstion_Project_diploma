c667e01e31b379d691f598d65f17051ec3e58240
valorswift <paau1508@gmail.com>
2026-04-14 20:47:07 +0530

    Update server2.js

--- server/server2.js
+++ server/server2.js
@@ -9,7 +9,9 @@ const cors = require("cors");
 const mysql = require("mysql2/promise");
 const bcrypt = require("bcryptjs");
 const path = require("path");
-require('dotenv').config();
+if (process.env.NODE_ENV !== "production") {
+  require('dotenv').config();
+}
 
 const app = express();
 
